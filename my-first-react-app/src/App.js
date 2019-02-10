4ba0266ae3e048246fab0896caf8433df49e1c2f
Amoses Holton <aholton001@gmail.com>
2019-02-10 10:15:08 -0500

    Creating functional component with dynamic content

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi I'm Amoses</h1>
+        <Person name="Moses" age="24"/>
+        <Person name="Christy" age="24"> My hobbies, reading DSM-5</Person>
+        <Person name="Andre" age="23"/>
       </div>
     );
   }
