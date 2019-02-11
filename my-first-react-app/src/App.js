4344d2da2455a958b7ba959551b091941e4db412
Amoses Holton <aholton001@gmail.com>
2019-02-10 19:49:40 -0500

    Using state manipulation

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            {name: 'Moses', age: 24},
+            {name: 'Christy', age: 24},
+            {name: 'Andre', age: 23},
+        ]
+    };
+
+    switchNameHandler = () => {
+        // console.log("Was clicked!");
+        // DON'T DO THIS: this.state.persons[0].name = "Amoses"
+        this.setState({
+            persons: [
+                {name: 'Amoses', age: 24},
+                {name: 'Christy', age: 24},
+                {name: 'Andre', age: 29},
+            ]
+        })
+    };
+
   render() {
     return (
       <div className="App">
         <h1>Hi I'm Amoses</h1>
-        <Person name="Moses" age="24"/>
-        <Person name="Christy" age="24"> My hobbies, reading DSM-5</Person>
-        <Person name="Andre" age="23"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> My hobbies, reading DSM-V</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
